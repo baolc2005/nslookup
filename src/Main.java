0918c41567c42546d6f28d217f0edca469caec2c
baro1chuo <lychibao200t@gmail.com>
2025-12-01 18:51:19 +0700

    Done chuc nang nslookup

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,225 @@
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.net.InetAddress;
 
-public class Main {
-    private JFrame mainFrame;
-    private JLabel headerLabel;
-    private JLabel statusLabel;
-    private JPanel controlPanel;
-    private JLabel msglabel;
-
+/**
+ *
+ * @author HP
+ */
+public class Main extends javax.swing.JFrame {
     public Main() {
-        prepareGUI();
+        initComponents();
+        setLocationRelativeTo(null);
     }
 
+    private void initComponents() {
+
+        JPanel jPanel1 = new JPanel();
+        JLabel jLabel_Domain = new JLabel();
+        jTextField_Domain = new javax.swing.JTextField();
+        JButton jButton_IP = new JButton();
+        JScrollPane jScrollPane1 = new JScrollPane();
+        jTextArea_KetQua = new javax.swing.JTextArea();
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu();
+        JMenuItem openMenuItem = new JMenuItem();
+        JMenuItem saveMenuItem = new JMenuItem();
+        JMenuItem saveAsMenuItem = new JMenuItem();
+        JMenuItem exitMenuItem = new JMenuItem();
+        JMenu editMenu = new JMenu();
+        JMenuItem cutMenuItem = new JMenuItem();
+        JMenuItem copyMenuItem = new JMenuItem();
+        JMenuItem pasteMenuItem = new JMenuItem();
+        JMenuItem deleteMenuItem = new JMenuItem();
+        JMenu helpMenu = new JMenu();
+        JMenuItem contentsMenuItem = new JMenuItem();
+        // Variables declaration - do not modify
+        JMenuItem aboutMenuItem = new JMenuItem();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel_Domain.setText("Tên miền:");
+
+        jTextField_Domain.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField_DomainKeyPressed(evt);
+            }
+        });
+
+        jButton_IP.setText("Tìm IP");
+        jButton_IP.addActionListener(evt -> jButton_IPActionPerformed(evt));
+        jButton_IP.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jButton_IPKeyPressed(evt);
+            }
+        });
+
+        jTextArea_KetQua.setEditable(false);
+        jTextArea_KetQua.setColumns(20);
+        jTextArea_KetQua.setRows(5);
+        jScrollPane1.setViewportView(jTextArea_KetQua);
+
+        fileMenu.setMnemonic('f');
+        fileMenu.setText("File");
+
+        openMenuItem.setMnemonic('o');
+        openMenuItem.setText("Open");
+        fileMenu.add(openMenuItem);
+
+        saveMenuItem.setMnemonic('s');
+        saveMenuItem.setText("Save");
+        fileMenu.add(saveMenuItem);
+
+        saveAsMenuItem.setMnemonic('a');
+        saveAsMenuItem.setText("Save As ...");
+        saveAsMenuItem.setDisplayedMnemonicIndex(5);
+        fileMenu.add(saveAsMenuItem);
+
+        exitMenuItem.setMnemonic('x');
+        exitMenuItem.setText("Exit");
+        exitMenuItem.addActionListener(evt -> exitMenuItemActionPerformed(evt));
+        fileMenu.add(exitMenuItem);
+
+        menuBar.add(fileMenu);
+
+        editMenu.setMnemonic('e');
+        editMenu.setText("Edit");
+
+        cutMenuItem.setMnemonic('t');
+        cutMenuItem.setText("Cut");
+        editMenu.add(cutMenuItem);
+
+        copyMenuItem.setMnemonic('y');
+        copyMenuItem.setText("Copy");
+        editMenu.add(copyMenuItem);
+
+        pasteMenuItem.setMnemonic('p');
+        pasteMenuItem.setText("Paste");
+        editMenu.add(pasteMenuItem);
+
+        deleteMenuItem.setMnemonic('d');
+        deleteMenuItem.setText("Delete");
+        editMenu.add(deleteMenuItem);
+
+        menuBar.add(editMenu);
+
+        helpMenu.setMnemonic('h');
+        helpMenu.setText("Help");
+
+        contentsMenuItem.setMnemonic('c');
+        contentsMenuItem.setText("Contents");
+        helpMenu.add(contentsMenuItem);
+
+        aboutMenuItem.setMnemonic('a');
+        aboutMenuItem.setText("About");
+        helpMenu.add(aboutMenuItem);
+
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jScrollPane1)
+                                .addContainerGap())
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(40, Short.MAX_VALUE)
+                                .addComponent(jLabel_Domain)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField_Domain, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton_IP)
+                                .addGap(29, 29, 29))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(47, 47, 47)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel_Domain)
+                                        .addComponent(jTextField_Domain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jButton_IP))
+                                .addGap(12, 12, 12)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 191, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>
+
+    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+        System.exit(0);
+    }
+
+    private void jButton_IPActionPerformed(java.awt.event.ActionEvent evt) {
+        String domain = jTextField_Domain.getText();
+        try {
+            InetAddress ipAddress = InetAddress.getByName(domain);
+            String ip = ipAddress.getHostAddress();
+            jTextArea_KetQua.setText("Địa chỉ IP: " + ip);
+        } catch (Exception e) {
+            jTextArea_KetQua.setText("Không tìm thấy địa chỉ IP của tên miền này" );
+        }
+
+    }
+
+    private void jButton_IPKeyPressed(java.awt.event.KeyEvent evt) {
+        if(evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            jButton_IPActionPerformed(null);
+        }
+    }
+
+    private void jTextField_DomainKeyPressed(java.awt.event.KeyEvent evt) {
+        if(evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            String domain = jTextField_Domain.getText();
+            try {
+                InetAddress ipAddress = InetAddress.getByName(domain);
+                String ip = ipAddress.getHostAddress();
+                jTextArea_KetQua.setText("Địa chỉ IP: " + ip);
+            } catch (Exception e) {
+                jTextArea_KetQua.setText("Không tìm thấy địa chỉ IP của tên miền này" );
+            }
+        }
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Main demo = new Main();
-        demo.showJPanelDemo();
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new Main().setVisible(true));
     }
 
-    private void prepareGUI() {
-        mainFrame = new JFrame("Vi du JPanel trong Java Swing");
-        mainFrame.setSize(500, 500);
-        mainFrame.setLayout(new GridLayout(3, 1));
-        mainFrame.setLocationRelativeTo(null);
-        headerLabel = new JLabel("", JLabel.CENTER);
-        statusLabel = new JLabel("", JLabel.CENTER);
-        statusLabel.setSize(500, 500);
-        msglabel = new JLabel("Chao mung ban den voi VietTuts.Vn", JLabel.CENTER);
-        controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
-        mainFrame.add(headerLabel);
-        mainFrame.add(controlPanel);
-        mainFrame.add(statusLabel);
-        mainFrame.setVisible(true);
-        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-
-    private void showJPanelDemo() {
-        headerLabel.setText("Container in action: JPanel");
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.add(msglabel);
-        controlPanel.add(panel);
-        mainFrame.setVisible(true);
-    }
+    private javax.swing.JTextArea jTextArea_KetQua;
+    private javax.swing.JTextField jTextField_Domain;
 }
